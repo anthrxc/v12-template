1752c36847d404f79d1e8af3b1817d3c305dd7d9
aanthr0 <anthromadayt@gmail.com>
2021-04-28 14:52:53 +0200

    Finished command handler

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -5,23 +5,25 @@ const { sucess, error } = require("log-symbols");
 module.exports = (client) => {
     var dir = `${process.cwd()}${sep}src${sep}commands`
     
-    client.loadCommands = () => {
-        readdir(dir, (err, files) => {
+    try {
+        readdir(dir, (err, subDirs) => {
             if(err) throw err;
-
-            files.forEach(subDir => {
+            
+            subDirs.forEach(subDir => {
                 dir = `${dir}${sep}${subDir}`
                 const commands = readdirSync(`${dir}`).filter(f => f.endsWith(".js"));
-
+                
                 for(const command of commands) {
                     dir = `${dir}${sep}${command}`
                     const cmd = require(`${dir}/${subDir}/${command}`);
 
                     if(cmd.help) {
                         if(!cmd.help.name) throw new TypeError(`${error} Command at ${dir} does not export a name value.`);
+                        if(!typeof cmd.help.name == "string") throw new TypeError(`${error} Command at ${dir} exports an invalid name value, it can only be a string.`);
                         if(!cmd.help.description) throw new TypeError(`${error} Command at ${dir} does not export a description value.`);
+                        if(!typeof cmd.help.description == "string") throw new TypeError(`${error} Command at ${dir} exports an invalid description value, it can only be a string.`);
+                        
                         if(client.commands.get(cmd.help.name)) throw new TypeError(`${error} Command at ${dir} exports the same name value as another command.`);
-
                         client.commands.set(cmd.help.name, cmd)
 
                         if(cmd.help.aliases) {
@@ -35,12 +37,59 @@ module.exports = (client) => {
                             else throw new TypeError(`${error} Command at ${dir} exports an invalid alias value. Value must be a string or object.`);
                         };
                         if(cmd.help.ownerOnly == true && cmd.help.requiredPerms) throw new TypeError(`${error} Command at ${dir} has a setting conflict: ownerOnly and requiredPerms cannot be used together.`);
-                        if(!typeof cmd.help.ownerOnly == "boolean") throw new TypeError(`${error} Command at ${dir} uses an invalid value for the ownerOnly setting.`);                        
+                        if(!typeof cmd.help.ownerOnly == "boolean") throw new TypeError(`${error} Command at ${dir} uses an invalid value for the ownerOnly setting.`);
+                        if(cmd.help.requiredPerms) {
+                            if(typeof cmd.help.requiredPerms == "string") cmd.help.requiredPerms = [cmd.help.requiredPerms]
+                            else if(typeof cmd.help.requiredPerms == "object") {
+                                cmd.help.requiredPerms.forEach(permission => {
+                                    const permissions = [
+                                        "ADMINISTRATOR",
+                                        "CREATE_INSTANT_INVITE",
+                                        "KICK_MEMBERS",
+                                        "BAN_MEMBERS",
+                                        "MANAGE_CHANNELS",
+                                        "MANAGE_GUILD",
+                                        "ADD_REACTIONS",
+                                        "VIEW_AUDIT_LOG",
+                                        "PRIORITY_SPEAKER",
+                                        "STREAM",
+                                        "VIEW_CHANNEL",
+                                        "SEND_MESSAGES",
+                                        "SEND_TTS_MESSAGES",
+                                        "MANAGE_MESSAGES",
+                                        "EMBED_LINKS",
+                                        "ATTACH_FILES",
+                                        "READ_MESSAGE_HISTORY",
+                                        "MENTION_EVERYONE",
+                                        "USE_EXTERNAL_EMOJIS",
+                                        "VIEW_GUILD_INSIGHTS",
+                                        "CONNECT",
+                                        "SPEAK",
+                                        "MUTE_MEMBERS",
+                                        "DEAFEN_MEMBERS",
+                                        "MOVE_MEMBERS",
+                                        "USE_VAD",
+                                        "CHANGE_NICKNAME",
+                                        "MANAGE_NICKNAMES",
+                                        "MANAGE_ROLES",
+                                        "MANAGE_WEBHOOKS",
+                                        "MANAGE_EMOJIS"
+                                    ]
+                                    if(!permissions.includes(permission.toUpperCase())) throw new TypeError(`${error} Command at ${dir} requires an invalid permission: ${permission}.`);
+                                });
+                            }
+                            else throw new TypeError(`${error} Command at ${dir} exports an invalid requiredPerms value. Value must be a string or object.`);
+                        };             
                     }
                     else throw new TypeError(`${error} Command at ${dir} does not export a help object.`);
-
                 }
             })
         })
+    }
+    catch(e) {
+        throw e
+    }
+    finally {
+        console.log(`${sucess} Loaded all commands!`);
     }
 }
