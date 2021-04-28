157b0e4752d2b1bb6e75e564545cfe241baa1ac0
aanthr0 <anthromadayt@gmail.com>
2021-04-28 15:28:53 +0200

    Created an event handler and commented all current files to explain what does what

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -10,35 +10,35 @@ module.exports = (client) => {
             
             subDirs.forEach(subDir => {
                 dir = `${dir}${sep}${subDir}`
-                const commands = readdirSync(`${dir}`).filter(f => f.endsWith(".js"));
+                const commands = readdirSync(`${dir}`).filter(f => f.endsWith(".js")); // Filter the commands variable so it only includes files that end with ".js"
                 
                 for(const command of commands) {
                     dir = `${dir}${sep}${command}`
-                    const cmd = require(`${dir}/${subDir}/${command}`);
+                    const cmd = require(`${dir}/${subDir}/${command}`); // Require the command file
 
-                    if(cmd.help) {
-                        if(!cmd.help.name) throw new TypeError(`Command at ${dir} does not export a name value.`);
-                        if(!typeof cmd.help.name == "string") throw new TypeError(`Command at ${dir} exports an invalid name value, it can only be a string.`);
-                        if(!cmd.help.description) throw new TypeError(`Command at ${dir} does not export a description value.`);
-                        if(!typeof cmd.help.description == "string") throw new TypeError(`Command at ${dir} exports an invalid description value, it can only be a string.`);
-                        
+                    if(cmd.help) { // Check if the command file exports a help object
+                        if(!cmd.help.name) throw new TypeError(`Command at ${dir} does not export a name value.`);                                                               // These lines simply check
+                        if(!typeof cmd.help.name == "string") throw new TypeError(`Command at ${dir} exports an invalid name value, it can only be a string.`);                  // if the help object exports any
+                        if(!cmd.help.description) throw new TypeError(`Command at ${dir} does not export a description value.`);                                                 // values that our dynamic help command
+                        if(!typeof cmd.help.description == "string") throw new TypeError(`Command at ${dir} exports an invalid description value, it can only be a string.`);    // requires to display the command help
+                                                                                                                                                                                 // and if not, it throws an error.
                         if(client.commands.get(cmd.help.name)) throw new TypeError(`Command at ${dir} exports the same name value as another command.`);
-                        client.commands.set(cmd.help.name, cmd)
+                        client.commands.set(cmd.help.name, cmd); // Add the command to the collection of commands defined at index.js:5:1
 
                         if(cmd.help.aliases) {
-                            if(typeof cmd.help.aliases == "string") cmd.help.aliases = [cmd.help.aliases]
+                            if(typeof cmd.help.aliases == "string") cmd.help.aliases = [cmd.help.aliases] // If the exported alias is a string, convert it to an array with the only element being that string
                             else if(typeof cmd.help.aliases == "object") {
-                                cmd.help.aliases.forEach(alias => {
+                                cmd.help.aliases.forEach(alias => { // Loop through all aliases and see if another command already has that alias
                                     if(client.aliases.get(alias)) throw new TypeError(`Command at ${dir} exports the same alias value as another command.`);
-                                    client.aliases.set(alias, cmd.help.name);
+                                    client.aliases.set(alias, cmd.help.name); // if not, add the alias to the collection of aliases defined at index.js:5:1
                                 });
                             }
-                            else throw new TypeError(`Command at ${dir} exports an invalid alias value. Value must be a string or object.`);
+                            else throw new TypeError(`Command at ${dir} exports an invalid alias value. Value must be a string or object.`); // If the type of the alias value isn't a string or an object, throw an error
                         };
-                        if(cmd.help.ownerOnly == true && cmd.help.requiredPerms) throw new TypeError(`Command at ${dir} has a setting conflict: ownerOnly and requiredPerms cannot be used together.`);
-                        if(!typeof cmd.help.ownerOnly == "boolean") throw new TypeError(`Command at ${dir} uses an invalid value for the ownerOnly setting.`);
+                        if(cmd.help.ownerOnly == true && cmd.help.requiredPerms) throw new TypeError(`Command at ${dir} has a setting conflict: ownerOnly and requiredPerms cannot be used together.`); // Owners already have full permission over the bot, if a command is "ownerOnly", it shouldn't require any additional permissions
+                        if(!typeof cmd.help.ownerOnly == "boolean") throw new TypeError(`Command at ${dir} uses an invalid value for the ownerOnly setting.`); // the ownerOnly setting can only be a boolean -- true or false
                         if(cmd.help.requiredPerms) {
-                            if(typeof cmd.help.requiredPerms == "string") cmd.help.requiredPerms = [cmd.help.requiredPerms]
+                            if(typeof cmd.help.requiredPerms == "string") cmd.help.requiredPerms = [cmd.help.requiredPerms] // read line 29 -- instead of aliases, this is done to required permissions
                             else if(typeof cmd.help.requiredPerms == "object") {
                                 cmd.help.requiredPerms.forEach(permission => {
                                     const permissions = [
@@ -74,19 +74,19 @@ module.exports = (client) => {
                                         "MANAGE_WEBHOOKS",
                                         "MANAGE_EMOJIS"
                                     ]
-                                    if(!permissions.includes(permission.toUpperCase())) throw new TypeError(`Command at ${dir} requires an invalid permission: ${permission}.`);
+                                    if(!permissions.includes(permission.toUpperCase())) throw new TypeError(`Command at ${dir} requires an invalid permission: ${permission}.`); // Loop through a list of valid Discord permissions and see whether or not the exported permission is in that list
                                 });
                             }
-                            else throw new TypeError(`Command at ${dir} exports an invalid requiredPerms value. Value must be a string or object.`);
+                            else throw new TypeError(`Command at ${dir} exports an invalid requiredPerms value. Value must be a string or object.`); // read line 36 -- instead of aliases, this is done to required permissions
                         };             
                     }
-                    else throw new TypeError(`Command at ${dir} does not export a help object.`);
+                    else throw new TypeError(`Command at ${dir} does not export a help object.`); // Throw an error if there is no help object in a command
                 }
             })
         })
     }
     catch(e) {
-        throw e
+        throw e // If an error is encountered, throw it
     }
     finally {
         console.log(`Loaded all commands!`);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone" });
 
-client.config = require("./config.js");
-client.commands = new Collection();
-client.aliases = new Collection();
+client.config = require("./config.js"); // Make all variables in the config.js file available through client.config
+["commands", "aliases"].forEach(x => client[x] = new Collection()); // Create collections for the bot commands and their aliases
 
 require("./handlers/command.js")(client);
+require("./handlers/event.js")(client);
 
-client.login(client.config.token)
+client.login(client.config.token);
