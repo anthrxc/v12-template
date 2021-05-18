a0a2cc845d2d7ee1e59b7d089a55ab2d15578e0c
BDWNAV <rs736611@gmail.com>
2021-05-18 20:37:36 +1000

    mongoose_added

--- src/config-example.js
+++ src/config-example.js
@@ -5,6 +5,7 @@ module.exports = {
         uri: "DATABASE://URI:HERE" // if you use quick.db, delete this line
     },
     token: "BOT-TOKEN-HERE",
+    mongoose_uri: "YOUR-MONGOOSE-URI-STRING",
     prefix: "PREFIX-HERE",
     color: {
         positive: "GREEN", // #2ECC71
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone" });
+const mongoose = require("mongoose");
 
 client.config = require("./config.js"); // Make all variables in the config.js file available through client.config
 ["commands", "aliases"].forEach(x => client[x] = new Collection()); // Create collections for the bot commands and their aliases
 
 require("./handlers/command.js")(client); // require the handler and provide the client variable
 require("./handlers/event.js")(client);
+
+mongoose.connect(client.config.mongoose_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 client.login(client.config.token);
